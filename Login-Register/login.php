17149589663e32de2482ccc4427ec6c9652c2150
Erdem Sert <erdem.sert@ug.bilkent.edu.tr>
2021-12-28 21:22:32 +0300

    registration page html css is done

--- Login-Register/login.php
+++ Login-Register/login.php
@@ -34,7 +34,7 @@ if(!(empty($_POST))){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="../style/login.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../style/loginRegister.css" media="screen">
   <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
